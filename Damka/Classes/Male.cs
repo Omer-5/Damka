132f773052bf2f5cea6be637ed1172c84369610d
Niv Ozer <niv.ozer@yahoo.com>
2022-05-15 17:43:13 +0300

    showAvailable moves

--- Damka/Classes/Male.cs
+++ Damka/Classes/Male.cs
@@ -65,31 +65,73 @@ namespace Damka.Classes
         public List<int> getAvailableMoves(List<Button> board, int startIndex)
         {
             List<int> result = new List<int>();
-
+            //Position pos = new Position(startIndex);
 
             if (_color == Constants.PlayerColor.Black)
             {
-                if (board[startIndex - 9].Image == null)
-                { // Black can go left
-                    result.Add(startIndex - 9);
-                }
-                if (board[startIndex - 7].Image == null)
-                { // Black can go right
-                    result.Add(startIndex - 7);
+                if (isValidMove(startIndex, startIndex - 7))
+                {
+                    if (board[startIndex -7].Image == null)
+                    { // black can go right
+                        result.Add(startIndex - 7);
+                    }
+                    else
+                    {
+                        if (isValidMove(startIndex - 9, startIndex -18))
+                        {
+                            if (board[startIndex - 18].Image == null)
+                            { // black can eat left
+                                result.Add(startIndex - 18);
+                            }
+                        }
+                    }
                 }
             }
             else
             {
-                if (board[startIndex + 7].Image == null)
-                { // White can go left
-                    result.Add(startIndex + 7);
+                if (isValidMove(startIndex, startIndex + 7))
+                {
+                    if (board[startIndex + 7].Image == null)
+                    { // White can go left
+                        result.Add(startIndex + 7);
+                    }
+                    else
+                    {
+                        if(isValidMove(startIndex+7, startIndex + 14))
+                        {
+                            if (board[startIndex + 14].Image == null)
+                            { // White can eat left
+                                result.Add(startIndex + 14);
+                            }
+                        }
+                    }
                 }
-                if (board[startIndex + 9].Image == null)
-                { // White can go right
-                    result.Add(startIndex + 9);
+                if (isValidMove(startIndex, startIndex + 9))
+                {
+                    if (board[startIndex + 9].Image == null)
+                    { // White can go right
+                        result.Add(startIndex + 9);
+                    }
+                    else
+                    {
+                        if (isValidMove(startIndex + 9, startIndex + 18))
+                        {
+                            if (board[startIndex + 18].Image == null)
+                            { // White can eat right
+                                result.Add(startIndex + 18);
+                            }
+                        }
+                    }
                 }
             }
             return result;
+        }
+        public bool isValidMove(int startIndex,int desiredLocationIndex)
+        {
+            if (startIndex % Constants.NUM_OF_COLS == 0 && desiredLocationIndex % 8 == 7) return false;
+            if (startIndex % Constants.NUM_OF_COLS == 7 && desiredLocationIndex % 8 == 0) return false;
+            if (desiredLocationIndex >= Constants.NUM_OF_COLS * Constants.NUM_OF_ROWS || desiredLocationIndex <= 0) return false;
+            return true;
         }
     }
 }
